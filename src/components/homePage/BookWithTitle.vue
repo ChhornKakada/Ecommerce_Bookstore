182d671398798091afd3e7c2cdbb17f5a4d251d0
Chhorn Kakada <lokpozzchhornkakada@gmail.com>
2023-03-23 08:23:21 +0700

    fix bug

--- src/components/homePage/BookWithTitle.vue
+++ src/components/homePage/BookWithTitle.vue
@@ -12,8 +12,8 @@ defineProps({
 </script>
 
 <template>
-  <div>
-    <img :src="imgPath " alt="" class="shadow-xl rounded-md w-[220px] sm:w-[150px] md:w-[180px] lg:w-[220px] xl:w-[280px] 2xl:w-[320px] aspect-[1/1.41] object-cover">
+  <div class="w-[220px] sm:w-[150px] md:w-[180px] lg:w-[220px] xl:w-[280px] 2xl:w-[320px]">
+    <img :src="imgPath " alt="" class="shadow-xl rounded-md  aspect-[1/1.41] object-cover">
     <center>
       <h2 class="my-2 text-[1.2rem] tracking-wider">{{ title }}</h2>
     </center>
